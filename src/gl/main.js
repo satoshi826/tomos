77e3ba2729894398476586e0b0560e3e7bdf297a
satoshi hata <stosto826@gmail.com>
2023-05-15 18:06:34 +0900

    forPublic

--- src/gl/main.js
+++ src/gl/main.js
@@ -1,13 +1,8 @@
 import {Camera} from '../lib/engine/Camera'
-import {Geometory} from '../lib/engine/geometory'
-import {Mesh} from '../lib/engine/Mesh'
 import {Animation} from '../lib/engine/animation'
-import {geo} from '../lib/engine/asset/geometory/geometory'
 import {sendState} from '../lib/engine/function/state'
 import {cameraControl} from '../lib/engine/extend/mouse'
-import {setHandler} from '../lib/engine/function/state'
-import {standart, getDeferredRenderer} from '../lib/engine/extend/deferred'
-import {PointLight} from '../lib/engine/Light'
+import {getDeferredRenderer} from '../lib/engine/extend/deferred'
 import {lightRoom} from './tmp/lightRoom'
 
 export async function main(core) {
@@ -20,29 +15,6 @@ export async function main(core) {
     fovy    : 80,
   })
   cameraControl(camera)
-
-  // const box = new Geometory(core, geo.cube())
-  // const playerMaterial = standart(core, {color: [0.05, 0.05, 0.1], emission: 0.2})
-  // const playerLight = new PointLight({intensity: 10000, exponent: 2})
-  // const player = new Mesh(core, {geometory: box, material: playerMaterial, scale: [2, 5, 2], position: [0, 0, 0]})
-  // player.add(playerLight)
-
-  // const moveControl = (mesh) => {
-  //   setHandler('KeyW', () => {
-  //     mesh.mutate('position', ([x, y, z]) => [x, y, z - 2])
-  //   })
-  //   setHandler('KeyS', () => {
-  //     mesh.mutate('position', ([x, y, z]) => [x, y, z + 2])
-  //   })
-  //   setHandler('KeyD', () => {
-  //     mesh.mutate('position', ([x, y, z]) => [x + 2, y, z])
-  //   })
-  //   setHandler('KeyA', () => {
-  //     mesh.mutate('position', ([x, y, z]) => [x - 2, y, z])
-  //   })
-  // }
-
-  // moveControl(player)
 
   let target = {}
   const room = lightRoom(core, target)
